c467ec3cfde8be65b3549fb049bc54956b89e6dc
aogunrin chromebook <ogunrindeayomide18@gmail.com>
2026-04-25 12:30:55 +0100

    My second Golang file commit

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 func main() {
+	fmt.Println(CountAlpha("Hello123"))
+	fmt.Println(CountAlpha("12345"))
 	fmt.Println(CheckNumber("Hello123"))
-	fmt.Println(CheckNumber("Hello"))
 }
