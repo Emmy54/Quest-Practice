3a55b9f9b440bcf2c735e5862a7d7a1237be1c7b
aogunrin chromebook <ogunrindeayomide18@gmail.com>
2026-04-29 04:22:42 +0100

    frpm my chromebook

--- repeatalpha.go
+++ repeatalpha.go
@@ -7,12 +7,12 @@ func RepeatAlpha(str string) string {
 	result := ""
 	for _, char := range str {
 		if char >= 'A' && char <= 'Z' {
-			repeatcount := int(char - 'A' + 1)
+			repeatcount := int(char - 'A' + 1) // 'A' is 65 in ASCII, so we subtract 64 to get the correct repeat count
 			for i := 0; i < repeatcount; i++ {
 				result += string(char)
 			}
 		} else if char >= 'a' && char <= 'z' {
-			repeatcount := int(char - 'a' + 1)
+			repeatcount := int(char - 'a' + 1) // 'a' is 97 in ASCII, so we subtract 96 to get the correct repeat count
 			for i := 0; i < repeatcount; i++ {
 				result += string(char)
 			}
